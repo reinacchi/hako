86295e80456a040bf22edddb79a63722866c8904
Reina <notmarx.tech@gmail.com>
2025-07-13 13:43:47 +0700

    feat(database): Increase timeout threshold

--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -5,7 +5,10 @@ export default defineNitroPlugin(async () => {
     const config = useRuntimeConfig();
     mongoose.set("strictQuery", false);
 
-    await mongoose.connect(config.MongoDB)
+    await mongoose.connect(config.MongoDB, {
+        connectTimeoutMS: 30000,
+        socketTimeoutMS: 60000,
+    })
         .then(() => console.log("Connected to MongoDB"))
         .catch((err) => console.error(err));
 });
